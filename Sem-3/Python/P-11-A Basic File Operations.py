999dda54cbc5654a962bb6125bb2fb91ba4da6dd
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-09-26 16:31:19 +0530

    Add basic file operations program in Python
    Implemented basic file operations including creating, reading, and writing to a text file.

---
+++ Sem-3/Python/P-11-A Basic File Operations.py
@@ -0,0 +1,12 @@
+# P-11-A Write a program to perform the below operations on files:
+# ● Create a text file and write a string to it.
+# ● Read an entire text file. 
+# ● Read a text file line by line. 
+# ● Write a string to a file. 
+# ● Write a list of strings to a file. 
+# ● Count the number of lines, words in a file.
+
+%%writefile "test.txt"
+hello
+this is my first lines
+this is my Second lines
