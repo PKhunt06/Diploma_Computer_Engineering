5b61ec65859f3f3f1473965defc36388ce16ba88
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-09-26 16:32:30 +0530

    Add alphabet occurrences counting program
    Implemented a program to count alphabet occurrences in a file.

---
+++ Sem-3/Python/P-11-B Alphabet Occurrences in File.py
@@ -0,0 +1,5 @@
+# P-11-B Write a program that reads a text file and counts the occurrences of each alphabet in the file. The program should prompt the user to enter the filename.
+
+f=open("test.txt","r")
+print(f.read())
+f.close()
