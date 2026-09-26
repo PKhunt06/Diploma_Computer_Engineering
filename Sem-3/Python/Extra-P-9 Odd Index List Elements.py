1b073eb093a6b00e3d3115fd0f09f089e2e3b2a8
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-09-26 16:59:25 +0530

    Add script to display elements at odd index positions

---
+++ Sem-3/Python/Extra-P-9 Odd Index List Elements.py
@@ -0,0 +1,5 @@
+# Extra-P-9 Use a loop to display elements from a given list present at odd index positions.
+
+my_list = [10, 20, 30, 40, 50, 60, 70]
+for i in range(1, len(my_list), 2):
+    print(my_list[i])
