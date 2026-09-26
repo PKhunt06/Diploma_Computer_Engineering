80b8888078849a0fcc52a5f60fba4a3574b18d10
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-09-26 15:02:03 +0530

    Implement number pattern printing program
    Added a program to print a number pattern from 1 to n.

---
+++ Sem-3/Python/P-5-D Number Pattern.py
@@ -0,0 +1,17 @@
+# P-5-D Write programs to print below patterns:
+# 1
+# 1 2
+# 1 2 3
+# 1 2 3 4
+# 1 2 3 4 5
+
+def numpat(n):
+    num = 1
+    for i in range(0, n):
+        num = 1
+        for j in range(0, i+1):
+            print(num, end=" ")
+            num = num + 1
+        print("")
+n = 5
+numpat(n)
