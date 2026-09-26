3ca8f94c07e1faac0c0271c5a8c161c962414a94
Khunt Parikshit <163092842+PKhunt06@users.noreply.github.com>
2026-09-26 14:01:47 +0530

    Add input-output program for user details
    The program prompts the user for their name, contact number, email, address, and birthdate, then prints these details.

---
+++ Sem-3/Python/P-2 Input-Output.py
@@ -0,0 +1,13 @@
+# P-2 Write a program to read your name, contact number, email, and birthdate and print those details on the screen.
+
+name =  input("Enter your name             :- ")
+email = input("Enter your email            :- ")
+contact = input("Enter your contact number   :- ")
+adress = input("Enter your adress          :- ")
+birth = input("Enter your birth date      :- ")
+print("\n Here your details \n")
+print(name)
+print(email)
+print(contact)
+print(adress)
+print(birth)
